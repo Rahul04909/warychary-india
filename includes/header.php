3931d21510d80260a4ccc0593b081dbd69251afe
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-06 13:01:16 +0530

    updated

--- includes/header.php
+++ includes/header.php
@@ -29,11 +29,6 @@
                 </a>
             </div>
 
-            <!-- Mobile Toggle -->
-            <button class="mobile-toggle" id="mobile-menu-btn">
-                <i class="fas fa-bars"></i>
-            </button>
-
             <!-- Navigation -->
             <nav class="navbar" id="navbar">
                 <ul>
@@ -56,6 +51,11 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="1" y="3" width="15" height="13"></rect><polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon><circle cx="5.5" cy="18.5" r="2.5"></circle><circle cx="18.5" cy="18.5" r="2.5"></circle></svg>
                 </a>
             </div>
+
+            <!-- Mobile Toggle -->
+            <button class="mobile-toggle" id="mobile-menu-btn">
+                <i class="fas fa-bars"></i>
+            </button>
         </div>
     </div>
 </header>
