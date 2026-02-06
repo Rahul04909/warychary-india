b45153f9e0638aa6988bf9f2a4c9a63bf9955f45
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-06 12:44:44 +0530

    updated header

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,24 @@
+<!-- Topbar -->
+<div class="topbar">
+    <div class="container">
+        <div class="topbar-content">
+            <div class="topbar-left">
+                <p>Your One-Stop Solution Design Development Manufacturing</p>
+            </div>
+            <div class="topbar-center">
+                <a href="tel:+917988472662"><i class="fas fa-phone-alt"></i> +91-7988472662</a>
+                <a href="mailto:Support@WaryChary.com"><i class="fas fa-envelope"></i> Support@WaryChary.com</a>
+            </div>
+            <div class="topbar-right">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <header class="header-main">
     <div class="container">
         <div class="header-wrapper">
