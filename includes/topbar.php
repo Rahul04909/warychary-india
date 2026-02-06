cff42328813332dfc570f8ffb15e73855ed887d9
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-06 13:21:42 +0530

    updated header topbar

---
+++ includes/topbar.php
@@ -0,0 +1,20 @@
+<!-- Topbar -->
+<div class="topbar">
+    <div class="container">
+        <div class="topbar-content">
+            <div class="topbar-left">
+                <p>Care With Awareness</p>
+            </div>
+            <div class="topbar-center">
+                <a href="tel:+917988472662"><i class="fas fa-phone-alt"></i> +91-9813716032</a>
+                <a href="mailto:Support@WaryChary.com"><i class="fas fa-envelope"></i> Support@WaryChary.com</a>
+            </div>
+            <div class="topbar-right">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
