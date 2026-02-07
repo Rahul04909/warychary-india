055027b6c9663a4b716af47dc16a86af58d67e00
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-07 14:37:14 +0530

    updated urls

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -12,6 +12,9 @@
     </main> <!-- End admin-main -->
 </div> <!-- End admin-wrapper -->
 
+<!-- Bootstrap 5 JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 <!-- Admin JS -->
 <script src="<?php echo $url_prefix; ?>assets/js/admin.js?v=<?php echo time(); ?>"></script>
 </body>
--- admin/includes/header.php
+++ admin/includes/header.php
@@ -14,6 +14,9 @@ if (!isset($url_prefix)) $url_prefix = '';
     
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     
     <!-- Admin CSS -->
     <link rel="stylesheet" href="<?php echo $url_prefix; ?>assets/css/admin.css?v=<?php echo time(); ?>">
