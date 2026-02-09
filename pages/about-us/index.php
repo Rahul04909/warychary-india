3b77e4c855b392ad1ff3eaf0b450857b45b16a25
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-09 18:42:53 +0530

    updated cdn links

--- pages/about-us/index.php
+++ pages/about-us/index.php
@@ -51,6 +51,7 @@ $page_title = "About Us - WaryChary";
     </script>
 
     <!-- Stylesheets -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="<?php echo $url_prefix; ?>assets/css/style.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="<?php echo $url_prefix; ?>assets/css/topbar.css?v=<?php echo time(); ?>">
@@ -275,5 +276,6 @@ $page_title = "About Us - WaryChary";
 </section>
 
 <?php include_once $url_prefix . 'includes/footer.php'; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
