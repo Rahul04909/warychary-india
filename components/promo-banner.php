1400581d679567aa34eff47c7383b22ddbfda2e4
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-16 11:11:23 +0530

    updated promo banner

--- components/promo-banner.php
+++ components/promo-banner.php
@@ -1,7 +1,7 @@
 <section class="promo-banner-section">
     <div class="container-fluid p-0"> <!-- Full width -->
         <a href="products.php" class="promo-banner-wrapper">
-            <img src="../assets/images/banner-1.png" alt="Enjoy 20% Off Your First Buy - Use Code: First20" class="promo-banner-img">
+            <img src="../assets/images/banner-2.png" alt="Enjoy 20% Off Your First Buy - Use Code: First20" class="promo-banner-img">
         </a>
     </div>
 </section>
