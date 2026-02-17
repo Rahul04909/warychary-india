f181d7003048fb0b47f2cce0d9094a9f7e16e189
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-17 21:36:51 +0530

    updated

--- admin/orders/courier-receipt.php
+++ admin/orders/courier-receipt.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 session_start();
 // Admin Auth
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
